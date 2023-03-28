73c6a64f125f6e0c774c712a029e876b7297c6ae
novitskynick <novitskynick@gmail.com>
2023-03-28 15:05:13 +0300

    add advice controller for some exception handling

--- src/main/java/app/exceptions/GlobalExceptionHandlingControllerAdvice.java
+++ src/main/java/app/exceptions/GlobalExceptionHandlingControllerAdvice.java
@@ -33,7 +33,7 @@ public class GlobalExceptionHandlingControllerAdvice {
         mav.addObject("exception", exception);
         mav.addObject("url", req.getRequestURL());
         mav.addObject("timestamp", new Date().toString());
-        mav.addObject("status", 500);
+        mav.addObject("status", 400);
         mav.setViewName("error");
         return mav;
     }
